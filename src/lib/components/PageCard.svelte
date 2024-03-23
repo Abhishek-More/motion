b01723f45d1d1269bd40829021750b7f0fc8779f
skandrigi <skandrigi@gmail.com>
2024-03-23 14:14:26 -0500

    add transition animation

--- src/lib/components/PageCard.svelte
+++ src/lib/components/PageCard.svelte
@@ -17,7 +17,7 @@
 </script> 
 
 
-<button on:click={() => clicker()} class="border border-black p-4 w-[250px] h-[200px] rounded-md">
+<button on:click={() => clicker()} class="border border-black p-4 w-[250px] h-[200px] rounded-md hover:bg-black hover:text-white transition-all">
   <p class="font-bold text-xl truncate">{title}</p>
   <p>{description}</p>
 </button>
