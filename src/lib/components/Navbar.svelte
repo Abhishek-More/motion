88ed4db5d517410de50f78739f6d34d1363fc816
skandrigi <skandrigi@gmail.com>
2024-03-23 12:46:55 -0500

    target blank for github

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,6 +2,6 @@
   <h1 class="font-semibold text-4xl">Motion 💨</h1>
   <div class="items-end">
     <a href="/lobby?teacher=1" class="border space-x-20 hover:text-black text-white	bg-black hover:bg-white px-6 py-2 rounded-md min-w-[200px] text-center ">Host Lobby</a>
-    <a href="https://github.com/Abhishek-More/motion"  class="border rounded-md px-4 py-2 hover:text-white	bg-white hover:bg-blue-500 transition-all">View on GitHub</a>
+    <a href="https://github.com/Abhishek-More/motion"  target = "_blank"class="border rounded-md px-4 py-2 hover:text-white	bg-white hover:bg-blue-500 transition-all">View on GitHub</a>
   </div>
 </div>
