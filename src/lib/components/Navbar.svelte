9681ff6d4d9d724b4f42c34d44e60325f744e9ba
Abhishek More <abhimore74@gmail.com>
2024-03-23 11:08:54 -0500

    frontend

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,4 @@
+<div class="flex items-center justify-between w-full px-8 mt-2">
+  <h1 class="font-semibold text-lg">Motion 💨</h1>
+  <a href="https://github.com/Abhishek-More/motion" class="border rounded-md px-4 py-2 bg-white hover:bg-blue-300 transition-all">View on GitHub</a>
+</div>
---
+++ src/lib/components/PageCard.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let title = '';
+  export let description = '';
+  export let link = ""
+
+</script> 
+
+<a href={`/game/${link}`} class="border border-black p-4 w-[250px] h-[200px] rounded-md">
+  <p class="font-bold text-xl">{title}</p>
+  <p>{description}</p>
+</a>
