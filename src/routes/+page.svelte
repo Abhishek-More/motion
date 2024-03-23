561f64db41a6523fab7da46fe1faafe22ac85c82
skandrigi <skandrigi@gmail.com>
2024-03-23 12:17:39 -0500

    frontend 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
     import PageCard from "$lib/components/PageCard.svelte";
     import FaChevronDown from 'svelte-icons/fa/FaChevronDown.svelte';
     import * as Menubar from "$lib/components/ui/menubar";
+    import * as Select from "$lib/components/ui/select";
     
     let v = 0;
     let cate = {
@@ -34,7 +35,16 @@
     <p class="text-2xl font-semibold">Your pages</p>
     <div class="flex mt-4">
         <div class="flex">
-            <a href="/">All pages</a>
+            <Select.Root>
+                <Select.Trigger class="w-[180px]">
+                  <Select.Value placeholder="Theme" />
+                </Select.Trigger>
+                <Select.Content>
+                  <Select.Item value="light">Light</Select.Item>
+                  <Select.Item value="dark">Dark</Select.Item>
+                  <Select.Item value="system">System</Select.Item>
+                </Select.Content>
+              </Select.Root>
             <div class="w-3 h-3">
                 <FaChevronDown class="w-3 h-3" />
             </div>
