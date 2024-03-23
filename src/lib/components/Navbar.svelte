bed6140edf94d7d0d3a3dd903812a701b5720a93
skandrigi <skandrigi@gmail.com>
2024-03-23 14:19:53 -0500

    adjust logo size

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,5 @@
 <div class="flex items-center justify-between w-full px-8 mt-2 pt-2 pb-4 drop-shadow-sm">
-  <h1 class="font-semibold text-4xl">Motion 💨</h1>
+  <h1 class="font-semibold text-5xl">Motion 💨</h1>
   <div class="items-end gap-x-10">
     <a href="/lobby?teacher=1" class="drop-shadow-sm hover:drop-shadow-none border space-x-20 hover:text-black text-white	bg-black hover:bg-white px-6 py-2 rounded-md min-w-[200px] text-center">Host Lobby</a>
     <a href="https://github.com/Abhishek-More/motion" target = "_blank" class="drop-shadow-sm hover:drop-shadow-none border rounded-md px-4 py-2 hover:text-white	bg-white hover:bg-blue-500 transition-all">View on GitHub</a>
